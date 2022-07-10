8bd314a79bd1b01f3368d9246644582e9b28e854
Ganesh9585 <109009918+Ganesh9585@users.noreply.github.com>
2022-07-09 20:23:16 -0700

    Add files via upload

---
+++ Primenumber.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class Primenumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=21;
+		int check =0;
+		boolean isPrime = false;
+		
+		for (int i = 2; i < n-1; i++) {
+			check = n%i;
+			if (check==0) {
+				isPrime = true;
+				break;
+			}
+		}
+		if (isPrime == true) {
+			System.out.println("The number is not Prime");
+		} else {
+			System.out.println("The number is Prime");
+		}
+	}
+	
+
+			
+		
+
+	}
+
+
